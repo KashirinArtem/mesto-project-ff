1b79995ff11ed836412de2ea7445fd4043b1588d
Artem Kashirin <inbox@artemkashirin.ru>
2024-09-09 17:26:33 +0500

    remove listener to the createCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 // @todo: Темплейт карточки
 const TEMPLATE = "#card-template",
+  CARD = ".card",
   CARD_IMAGE = ".card__image",
   CARD_TITLE = ".card__title",
-  DELETE_BTN = "card__delete-button",
+  DELETE_BTN = ".card__delete-button",
   LIST = ".places__list";
 
 const templateCard = document.querySelector(TEMPLATE).content,
@@ -11,29 +12,28 @@ const templateCard = document.querySelector(TEMPLATE).content,
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-const createCard = (name, alt, link) => {
+const createCard = ({ name, alt, link }) => {
   const cardClone = templateCard.cloneNode(true),
     cardImg = cardClone.querySelector(CARD_IMAGE),
-    cardTitle = cardClone.querySelector(CARD_TITLE);
+    cardTitle = cardClone.querySelector(CARD_TITLE),
+    deleteBtn = cardClone.querySelector(DELETE_BTN);
 
   cardImg.src = link;
   cardImg.alt = alt;
   cardTitle.textContent = name;
+
+  deleteBtn.addEventListener("click", removeCard);
 
   return cardClone;
 };
 
 // @todo: Функция удаления карточки
 const removeCard = (event) => {
-  const target = event.target,
-    parentNode = target.parentNode;
-
-  [...target.classList].includes(DELETE_BTN) && parentNode.remove();
+  [...event.target.parentNode.classList].includes("card") &&
+    event.target.parentNode.remove();
 };
 
-placesList.addEventListener("click", removeCard);
-
 // @todo: Вывести карточки на страницу
-initialCards.forEach(({ name, alt, link }) => {
-  placesList.append(createCard(name, alt, link));
+initialCards.forEach((card) => {
+  placesList.append(createCard(card));
 });
